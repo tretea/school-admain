3f91874383494f0e19349da01b32e654266e2872
SunZhichao <2433469529@qq.com>
2022-01-24 19:07:03 +0800

    bug 查询班级 条件表id与详情id不符

--- index.py
+++ index.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 #coding=utf-8
+from dataclasses import dataclass
 import pymysql
 import os
 db=pymysql.connect(host='localhost',user='root',database='school',charset='utf8')
@@ -33,16 +34,17 @@ def FindGrade():
 # 查询班级
 def FindClass():
     sql='select class.Type,class.id,GradeName,ClassName,TeacherName,SubjectName from grades right join class on class.grade_id=grades.id left join teachers on teachers.id=class.teacher_id left join subjects on subjects.id=teachers.subject_id'
-    conditions=map(int,input('请输入查询条件(年级 0/文理科 1 (多个条件以空格分隔)):').strip().split(' '))
+    conditions=map(str,input('请输入查询条件(年级 0/文理科 1 (多个条件以空格分隔)):').strip().split(' '))
     clear()
     for i in conditions:
-        if i==0:
+        if i=='0':
            FindGrade() 
            gradeid=int(input('请输入年级id:'))
+           gradeid=changId('grades',gradeid)
            strtemp=' where' if sql.find('where')<0 else ' and'
            sql=sql+strtemp+f' class.grade_id={gradeid}'
-        elif i==1:
-            typeid=int(input('全科0/理科1/文科2'))
+        elif i=='1':
+            typeid=int(input('全科0/理科1/文科2:'))
             strtemp=' where' if sql.find('where')<0 else ' and'
             sql=sql+strtemp+f' class.Type={typeid}'
     sql=sql+';'
@@ -71,12 +73,50 @@ def FindClass():
 
 # 查看班级详情信息
 def FindClassDetail():
-    class_id=int(input('请输入班级id查询详情:'))
-    sql=f'select t5.id,t5.ClassName,t5.TeacherName,t5.t1name,t5.t2name,t5.t3name,t5.t4name,teachers.TeacherName as t5name from (select t4.id,t4.ClassName,t4.TeacherName,t4.t1name,t4.t2name,t4.t3name,teachers.TeacherName as t4name,t4.t5_id from (select t3.id,t3.ClassName,t3.TeacherName,t3.t1name,t3.t2name,teachers.TeacherName as t3name,t3.t4_id,t3.t5_id from (select t2.id,t2.ClassName,t2.TeacherName,t2.t1name,teachers.TeacherName as t2name,t2.t3_id,t2.t4_id,t2.t5_id from (select t1.id,t1.ClassName,t1.TeacherName,teachers.TeacherName as t1name,t1.t2_id,t1.t3_id,t1.t4_id,t1.t5_id from (select class.id,ClassName,TeacherName,t1_id,t2_id,t3_id,t4_id,t5_id from class left join teachers on class .teacher_id=teachers.id where class.id={class_id}) as t1 left join teachers on t1.t1_id=teachers.id) as t2 left join teachers on t2.t2_id=teachers.id) as t3 left join teachers on teachers.id=t3.t3_id) as t4 left join teachers on teachers.id=t4.t4_id) as t5 left join teachers on t5.t5_id=teachers.id;'
-    cursor.execute(sql)
-    data=cursor.fetchall()
-    for n in data:
-        print(n)
+    while True:
+        class_id=input('请输入班级id查询详情:')
+        if not class_id.isdigit():
+            break
+        else:
+            class_id=changId('class',int(class_id))
+            sql=f'select t5.id,t5.ClassName,t5.TeacherName,t5.t1name,t5.t2name,t5.t3name,t5.t4name,teachers.TeacherName as t5name from (select t4.id,t4.ClassName,t4.TeacherName,t4.t1name,t4.t2name,t4.t3name,teachers.TeacherName as t4name,t4.t5_id from (select t3.id,t3.ClassName,t3.TeacherName,t3.t1name,t3.t2name,teachers.TeacherName as t3name,t3.t4_id,t3.t5_id from (select t2.id,t2.ClassName,t2.TeacherName,t2.t1name,teachers.TeacherName as t2name,t2.t3_id,t2.t4_id,t2.t5_id from (select t1.id,t1.ClassName,t1.TeacherName,teachers.TeacherName as t1name,t1.t2_id,t1.t3_id,t1.t4_id,t1.t5_id from (select class.id,ClassName,TeacherName,t1_id,t2_id,t3_id,t4_id,t5_id from class left join teachers on class .teacher_id=teachers.id where class.id={class_id}) as t1 left join teachers on t1.t1_id=teachers.id) as t2 left join teachers on t2.t2_id=teachers.id) as t3 left join teachers on teachers.id=t3.t3_id) as t4 left join teachers on teachers.id=t4.t4_id) as t5 left join teachers on t5.t5_id=teachers.id;'
+            cursor.execute(sql)
+            datat=cursor.fetchone()
+            data=[]
+            for x in datat:
+                x='无数据' if not x else x
+                data.append(x)
+            sublist=[]
+            for n in data[2:]:
+                sql=f'select subjects.SubjectName from teachers left join subjects on teachers.subject_id=subjects.id where teachers.TeacherName="{n}";'
+                cursor.execute(sql)
+                sub=cursor.fetchone()
+                sub=('无数据',) if not sub else sub
+                sublist.append(sub[0])
+            print('-'*123)
+            print('id'.center(13,' '),end='|') 
+            print('班级'.center(13,' '),end='|')
+            print('班主任'.center(12,' '),end='|')
+            print(f'{sublist[0]}'.center(15-len(sublist[0]),' '),end='|')
+            print(f'{sublist[1]}'.center(15-len(sublist[1]),' '),end='|')
+            print(f'{sublist[2]}'.center(15-len(sublist[2]),' '),end='|')
+            print(f'{sublist[3]}'.center(15-len(sublist[3]),' '),end='|')
+            print(f'{sublist[4]}'.center(15-len(sublist[4]),' '))
+            print('-'*123)
+            print(f'{data[0]}'.center(15-len(str(data[0])),' '),end='|') 
+            print(f'{data[1]}'.center(15-len(data[1]),' '),end='|')
+            print(f'{data[2]}'.center(15-len(data[2]),' '),end='|')
+            print(f'{data[3]}'.center(15-len(data[3]),' '),end='|')
+            print(f'{data[4]}'.center(15-len(data[4]),' '),end='|')
+            print(f'{data[5]}'.center(15-len(data[5]),' '),end='|')
+            print(f'{data[6]}'.center(15-len(data[6]),' '),end='|')
+            print(f'{data[7]}'.center(15-len(data[7]),' '))
+            print('-'*123,'\n')
+            msg=input('是否继续查看详情 y?n ')
+            if msg=='y' or msg=='Y':
+                continue
+            else:
+                break
 # 查询老师
 def FindTeacher(typeid=0):
     if typeid==0:
@@ -85,17 +125,17 @@ def FindTeacher(typeid=0):
         cursor.execute(f'select teachers.id,ClassName,TeacherName,sex,age,SubjectName from (class right join teachers on teachers.id=class.teacher_id) left join subjects on teachers.subject_id=subjects.id where subjects.Type=0 or subjects.Type={typeid};')
     teachers=cursor.fetchall()
     print('老师'.center(129,'-'),'\n')
-    print('id'.center(20,' '),'姓名'.center(20,' '),'性别'.center(18,' '),'年龄'.center(19,' '),'学科'.center(20,' '),'班级'.center(20,' '))
+    print('id'.center(19,' '),'姓名'.center(18,' '),'性别'.center(18,' '),'年龄'.center(18,' '),'学科'.center(18,' '),'班级'.center(18,' '))
     print('-'*131)
     for idx,(ind,cl,name,sex,age,subject) in enumerate(teachers):
         cl=cl if cl else '未知'
         subject=subject if subject else '未知'
-        print(str(idx).center(20,' '),end='|')
-        print(name.center(20,' '),end='|')
-        print(sex.center(20,' '),end='|')
-        print(str(age).center(20,' '),end='|')
-        print(subject.center(20,' '),end='|')
-        print(cl.center(20,' '))
+        print(str(idx).center(19,' '),end='|')
+        print(name.center(20-len(name),' '),end='|')
+        print(sex.center(20-len(sex),' '),end='|')
+        print(str(age).center(19,' '),end='|')
+        print(subject.center(20-len(subject),' '),end='|')
+        print(cl.center(20-len(cl),' '))
         print('-'*131)
     print('\n')
 # 查询课程
@@ -110,9 +150,9 @@ def FindSubject():
             Type='主科'
         else:
             Type='理科' if Type==1 else '文科'
-        print(str(idx).center(20,' '),end='|')
-        print(subject.center(20,' '),end='|')
-        print(Type.center(20,' '))
+        print(str(idx).center(19,' '),end='|')
+        print(subject.center(20-len(subject),' '),end='|')
+        print(Type.center(20-len(Type),' '))
         print('-'*66)
     print('\n')
 
@@ -417,8 +457,7 @@ updateoptions='''
 **                                **
 ************************************
 '''
-FindClassDetail()
-while False:
+while True:
     clear()
     print(options)
     print('按其他任意键退出'.center(28,'-'),'\n')
@@ -441,6 +480,7 @@ while False:
                 input('按任意键返回'.center(34,'-'))
             elif int(findselect)==2:
                 FindClass()
+                FindClassDetail()
                 input('按任意键返回'.center(129,'-'))
             elif int(findselect)==3:
                 FindTeacher()
@@ -463,7 +503,7 @@ while False:
                 print('按其他任意键返回'.center(28,'-'),'\n')
             elif int(i)==2:
                 AddClass()
-                print('按其他任意键返回'.center(28,'-'),'\n')
+                print('按其他任意键返回'.center(28,'-'),'\n') 
             elif int(i)==3:
                 AddTeacher()
                 print('按其他任意键返回'.center(28,'-'),'\n')
